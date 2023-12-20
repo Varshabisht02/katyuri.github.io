1fb43d53115bf75fdbf64ec91865a94c2ceed728
varsha <bishtvarsha021@gmail.com>
2023-12-20 16:32:04 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 (function($) {
 
 	'use strict';
+ 
+  
+// 1. Proloder */
+
+    $(window).on('load', function () {
+      $('#preloader-active').delay(450).fadeOut('slow');
+      $('body').delay(450).css({
+        'overflow': 'visible'
+      });
+    });
+    
 
   $('.site-menu-toggle').click(function(){
     var $this = $(this);
